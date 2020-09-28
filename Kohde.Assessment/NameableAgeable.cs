9a24a37560c0e1e31dedd37d3233de50a8127f00
Chris Antonie Pieterse <chrisantoniep@gmail.com>
2020-09-28 21:02:15 +0200

    Add initial NameableAgeable abstract class

---
+++ Kohde.Assessment/NameableAgeable.cs
@@ -0,0 +1,12 @@
+﻿namespace Kohde.Assessment
+{
+    public abstract class NameableAgeable : INameableAgeable
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        public string GetDetails() =>  "Name: " + Name + "Age: " + Age;
+
+        public abstract override string ToString();
+    }
+}
