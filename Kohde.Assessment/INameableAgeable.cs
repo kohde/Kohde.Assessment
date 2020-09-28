c8d8328107ec04cf21add229b21d76abb033b6ad
Chris Antonie Pieterse <chrisantoniep@gmail.com>
2020-09-28 21:01:57 +0200

    Add intial INameableAgeable interface

---
+++ Kohde.Assessment/INameableAgeable.cs
@@ -0,0 +1,11 @@
+﻿namespace Kohde.Assessment
+{
+    public interface INameableAgeable
+    {
+        int Age { get; set; }
+        string Name { get; set; }
+
+        string GetDetails();
+        string ToString();
+    }
+}
