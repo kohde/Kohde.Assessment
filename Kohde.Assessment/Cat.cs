69b49774a13bd88e4351a7259a1c14e094bb7331
Rihan Louw <rihan@phinityrisk.com>
2023-01-22 21:52:29 +0200

    Made classes make use of new base class + created constructor + did override on GetDetails and ToString methods.

--- Kohde.Assessment/Cat.cs
+++ Kohde.Assessment/Cat.cs
@@ -1,14 +1,30 @@
 ﻿namespace Kohde.Assessment
 {
-    public class Cat
-    {
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public string Food { get; set; }
+  // made the class inherent from the new base class
+  // for less and cleaner code
+  public class Cat : Mammal.Mammal
+  {
+    public string Food { get; set; }
 
-        public string GetDetails()
-        {
-            return "Name: " + Name + "Age: " + Age;
-        }
+    // created a constructor for easier and cleaner declarations
+    public Cat(string name, int age, string food = null) : base(name, age)
+    {
+      Name = name;
+      Age = age;
+      Food = food;
     }
+
+    // made the method override the base classes' one
+    // to include the new property that is not in the base class
+    public override string GetDetails()
+    {
+      return $"Name: {Name}, Age: {Age}, Food: {Food}";
+    }
+
+    // override the ToString method to make it more meaningful
+    public override string ToString()
+    {
+      return GetDetails();
+    }
+  }
 }
--- Kohde.Assessment/Human.cs
+++ Kohde.Assessment/Human.cs
@@ -1,14 +1,30 @@
 ﻿namespace Kohde.Assessment
 {
-    public class Human
-    {
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public string Gender { get; set; }
+  // made the class inherent from the new base class
+  // for less and cleaner code
+  public class Human : Mammal.Mammal
+  {
+    public string Gender { get; set; }
 
-        public string GetDetails()
-        {
-            return "Name: " + Name + "Age: " + Age;
-        }
+    // created a constructor for easier and cleaner declarations
+    public Human(string name, int age, string gender = null) : base(name, age)
+    {
+      Name = name;
+      Age = age;
+      Gender = gender;
     }
+
+    // made the method override the base classes' one
+    // to include the new property that is not in the base class
+    public override string GetDetails()
+    {
+      return $"Name: {Name}, Age: {Age}, Gender: {Gender}";
+    }
+
+    // override the ToString method to make it more meaningful
+    public override string ToString()
+    {
+      return GetDetails();
+    }
+  }
 }
