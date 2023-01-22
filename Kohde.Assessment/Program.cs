94343afdb1a4cad9d099e6902b9dea2c24d93c2a
Rihan Louw <rihan@phinityrisk.com>
2023-01-22 22:22:19 +0200

    Assessment C - improve logic and provide defaults.

--- Kohde.Assessment/Program.cs
+++ Kohde.Assessment/Program.cs
@@ -219,14 +219,14 @@ namespace Kohde.Assessment
     public static int GetFirstEvenValue(List<int> numbers)
     {
       // RETURN THE FIRST EVEN NUMBER IN THE SEQUENCE
-      var first = numbers.Where(x => x % 2 == 0).First();
+      var first = numbers.Where(x => x % 2 == 0).FirstOrDefault();
       return first;
     }
 
     public static string GetSingleStringValue(List<string> stringList)
     {
       // THE OUTPUT MUST RENDER THE FIRST ITEM THAT CONTAINS AN 'a' INSIDE OF IT
-      var first = stringList.Where(x => x.IndexOf("a") != -1).Single();
+      var first = stringList.Where(x => x.IndexOf("a") != -1).SingleOrDefault();
       return first;
     }
 
