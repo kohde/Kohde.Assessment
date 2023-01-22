1c069123af2f530d96e16dffc3ed82272979b306
Rihan Louw <rihan@phinityrisk.com>
2023-01-22 21:50:54 +0200

    Created a new base class that implements the new interface.

---
+++ Kohde.Assessment/Mammal/Mammal.cs
@@ -0,0 +1,19 @@
+﻿namespace Kohde.Assessment.Mammal
+{
+  // created a base class for similar type of classes to inherit
+  // from + made the base class inherit from an interface to set
+  // requirements
+  public abstract class Mammal : IMammal
+  {
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    protected Mammal(string name, int age)
+    {
+      Name = name;
+      Age = age;
+    }
+
+    public abstract string GetDetails();
+  }
+}
